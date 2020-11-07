5ad6a3d7b77c8829b805238ee84622506a89ee4e
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-11-07 17:10:44 +0100

    MAPAPP-005: Navigate to activities - Add listener container with all workouts - Add _moveToPopup method which will move view to the chosen workout

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ class App {
     #map;
     #mapEvent;
     #workouts = [];
+    #mapZoomLevel = 13;
 
     constructor() {
         this._getPosition();
@@ -70,6 +71,7 @@ class App {
         // Event listeners
         form.addEventListener("submit", this._newWorkout.bind(this));
         inputType.addEventListener("change", this._toggleElevationForm.bind(this));
+        containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
     }
 
     _getPosition() {
@@ -83,7 +85,7 @@ class App {
         const coords = [latitude, longitude];
 
         // map is leaflet object
-        this.#map = L.map("map").setView(coords, 13);
+        this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
         L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -226,6 +228,19 @@ class App {
         }
 
         form.insertAdjacentHTML("afterend", html);
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest(".workout");
+        if (!workoutEl) return;
+
+        const workout = this.#workouts.find((work) => work.id === +workoutEl.dataset.id);
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1.5,
+            },
+        });
     }
 }
 
