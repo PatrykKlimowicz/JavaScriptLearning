a7115d653374fe54291b7ac2e27c16e121a87d11
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-11-12 22:06:39 +0100

    FORKIFY-001: Implement basic structure with MVC - Create model.js module - Create controller.js module - Create recipeView.js - Create basic flow with MVC and render first recipe - Use npm packages

---
+++ src/js/controller.js
@@ -0,0 +1,43 @@
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+
+// API DOCS PATH: https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+
+const controllRecipes = async function () {
+    try {
+        // render loadig spinner
+        recipeView.renderSpinner();
+
+        // get recipe id
+        const id = window.location.hash.slice(1);
+
+        if (!id) return;
+        // test id: 5ed6604591c37cdc054bc886
+
+        // load recipe
+        await model.loadRecipe(id);
+        let { recipe } = model.state;
+
+        // render recipe
+        recipeView.render(model.state.recipe);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const events = ["hashchange", "load"];
+events.forEach((ev) => window.addEventListener(ev, controllRecipes));
